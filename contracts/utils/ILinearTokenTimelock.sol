533597a7af9d7ad31d4a454bf4bd8eac073db142
freedomdao <83950030+freedomdao@users.noreply.github.com>
2021-05-11 20:20:16 +1200

    Add files

---
+++ contracts/utils/ILinearTokenTimelock.sol
@@ -0,0 +1,39 @@
+pragma solidity 0.6.0;
+pragma experimental ABIEncoderV2;
+
+/// Forked from Fei Protocol, see: https://github.com/fei-protocol/fei-protocol-core/blob/master/contracts/utils/ILinearTokenTimelock.sol
+/// @title LinearTokenTimelock interface
+interface ILinearTokenTimelock {
+    // ----------- Events -----------
+
+    event Release(address indexed _beneficiary, address indexed _recipient, uint256 _amount);
+    event BeneficiaryUpdate(address indexed _beneficiary);
+    event PendingBeneficiaryUpdate(address indexed _pendingBeneficiary);
+
+    // ----------- State changing api -----------
+
+    function release(address to, uint amount) external;
+
+    function releaseMax(address to) external;
+
+    function setPendingBeneficiary(address _pendingBeneficiary) external;
+
+    function acceptBeneficiary() external;
+
+
+    // ----------- Getters -----------
+
+    function lockedToken() external view returns (IERC20);
+
+    function beneficiary() external view returns (address);
+
+    function pendingBeneficiary() external view returns (address);
+
+    function initialBalance() external view returns (uint256);
+
+    function availableForRelease() external view returns (uint256);
+
+    function totalToken() external view returns(uint256);
+
+    function alreadyReleasedAmount() external view returns (uint256);
+}
